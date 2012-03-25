b195274efef11f7f9f9676c015c8a2c6f4631836
Kyle Spiegel <strike16k@yahoo.com>
2012-03-25 17:40:17 -0400

    Task 5

--- PrimeFactorization/src/PrimeGenerator.java
+++ PrimeFactorization/src/PrimeGenerator.java
@@ -7,10 +7,21 @@ public class PrimeGenerator {
 	public static ArrayList<Integer> generatePrimes (int n) {
 		ArrayList<Integer> ret = new ArrayList<Integer> ();
 		
-		
-		if(n > 2){
-			ret.add(2);	
+		int candidate = 2;
+		int divs = 0;
+	
+		while (candidate < n){
+			for(int f = 2; f < candidate; f++){
+				if (candidate % f == 0){
+					divs++;
+				}
+			}
+			if (divs == 0){
+				ret.add(candidate);
+			}
+			candidate++;
 		}
+	
 		
 		
 		return ret;
