6ae53f67f0dc4981ebf9aafae964a77d561df787
Kyle Spiegel <strike16k@yahoo.com>
2012-03-25 16:53:07 -0400

    First commitq

--- PrimeFactorization/src/PrimeNumbers.java
+++ PrimeFactorization/src/PrimeNumbers.java
@@ -3,7 +3,12 @@ import java.util.ArrayList;
 
 public class PrimeNumbers {
  public static ArrayList<Integer> generatePrimes (int n) {
+	 
+	ArrayList<Integer> ret = new ArrayList<Integer>();
 	
-	 return new ArrayList<Integer>();
+	if (n > 1){
+		ret.add(2);
+	}
+	return ret;
  }
 } 
