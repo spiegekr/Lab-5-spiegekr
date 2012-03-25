848e5be76aab4f6c96e68b670f9f97fd4cc1e34c
Kyle Spiegel <strike16k@yahoo.com>
2012-03-25 16:39:48 -0400

    First commit

---
+++ PrimeFactorization/src/PrimeFactorsTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+ 
+
+public class PrimeFactorsTest {
+
+@Test
+public void testJUnit() {
+	assertTrue ("The compiler is sick ",true );
+}
+}
