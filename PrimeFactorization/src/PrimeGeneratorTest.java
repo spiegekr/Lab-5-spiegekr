f1badcb36bab824a8a0377a1fa8d3ce4c0c1cbe9
Kyle Spiegel <strike16k@yahoo.com>
2012-03-25 17:15:58 -0400

    Task 5 start

---
+++ PrimeFactorization/src/PrimeGeneratorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class PrimeGeneratorTest {
+
+	@Test
+	public void testJUnit() {
+		assertTrue ("The compiler is sick ",true );
+	}
+
+}
